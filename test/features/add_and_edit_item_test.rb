5fbc569e7624026ee9ac6333203b3b974a952a05
Billy <navyosu@gmail.com>
2013-12-03 14:55:46 -0700

    Created test for creating item

---
+++ test/features/add_and_edit_item_test.rb
@@ -0,0 +1,34 @@
+require './test/test_helper'
+
+class AddAndEditItemTest < Capybara::Rails::TestCase
+
+  def test_stocker_can_create_an_item
+    visit root_path
+    assert page.has_content?('On the Menu At Ono')
+    # click_on "Sign up or Log in"
+
+    # within "#" do
+    #   fill_in "Email", with: 'benji@example.com'
+    #   fill_in "Password", with: 'password'
+    #   click_button ""
+    # end
+
+    # assert_content page, 'Logged in'
+    click_on "Dashboard"
+    assert page.has_content?("Stocker Dashboard")
+    click_on "Create New Item"
+    assert page.has_content?("Add a new menu item")
+    fill_in "Title", with: "Sample Item"
+    fill_in "Description", with: "Awesome"
+    fill_in "Price", with: "7"
+    select "Entrees", from: "Category"
+    assert page.has_content?("Retired?")
+    click_on "Create Item"
+
+    visit root_path
+
+    assert page.has_content?("Sample Item")
+
+  end
+
+end
