219322b207ac90a67fbbe54620d24494825e4c99
Ben Lewis <bennlewis@gmail.com>
2013-12-03 15:08:13 -0700

    added dashboard views and controllers from billys bbq

---
+++ app/controllers/admin_orders_controller.rb
@@ -0,0 +1,22 @@
+class AdminOrdersController < ApplicationController
+  # before_action :can_access
+  
+  def index
+    @orders = Order.all
+
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+
+    flash.notice = "Order number #{@order.id} removed!"
+
+    redirect_to admin_orders_path
+  end
+
+end
