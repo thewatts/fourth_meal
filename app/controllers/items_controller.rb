f0d4645b9414fdfb1d703404e3e4523921465d32
Ben Lewis <bennlewis@gmail.com>
2013-12-05 14:59:14 -0700

    completed branding, removed category filters, and updated relevant feature tests

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
-  before_action :load_category, :only => [:index, :in_category]
+  # before_action :load_category, :only => [:index, :in_category]
   layout "application"
 
-  def load_category
-    @categories = Category.all
-  end
+  # def load_category
+  #   @categories = Category.all
+  # end
 
   def index
     @restaurant = Restaurant.find_by_slug(params[:restaurant])
@@ -13,13 +13,13 @@ class ItemsController < ApplicationController
     @page_title = "Full Menu"
   end
 
-  def in_category
-    @restaurant = Restaurant.find_by_slug(params[:restaurant])
-    # @category = @restaurant.categories.find_by_slug(params[:category_slug])
-    @items = @category.items.active
-    @page_title = @category.title
-    render :index
-  end
+  # def in_category
+  #   @restaurant = Restaurant.find_by_slug(params[:restaurant])
+  #   # @category = @restaurant.categories.find_by_slug(params[:category_slug])
+  #   @items = @category.items.active
+  #   @page_title = @category.title
+  #   render :index
+  # end
 
   def update
     @item = Item.find(params[:id])
@@ -34,9 +34,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:title, :description, :price, :photo, :photo_file_name)
   end
 
-  def load_category
-    @categories = Category.all
-  end
+  # def load_category
+  #   @categories = Category.all
+  # end
 
 end
 
