dadf3c4f1c60b68fa569d77ba028124917e818a4
Billy <navyosu@gmail.com>
2013-12-07 11:38:46 -0700

    Used fixtures for users test, and added super column to user table to account for super admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131206221056) do
+ActiveRecord::Schema.define(version: 20131207174317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20131206221056) do
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "super",         default: false
   end
 
 end
