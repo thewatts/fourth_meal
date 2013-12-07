11c961e5f038de1397157407984246e0034ac3f5
Ben Lewis <bennlewis@gmail.com>
2013-12-07 10:08:52 -0700

    updated item category unit test to use fixtures

--- test/models/item_category_test.rb
+++ test/models/item_category_test.rb
@@ -3,23 +3,19 @@ require 'test_helper'
 class ItemCategoryTest < ActiveSupport::TestCase
   
   test "it is created with valid attributes" do
-    @item_category = ItemCategory.create(:item_id => 3, category_id: 5)
-    assert @item_category.valid?
-  end
-
-  test "it_validates_status" do
-    @item_category = ItemCategory.create()
-    assert @item_category.invalid?
+    assert item_categories(:one).valid?
   end
 
   test "it_validates_item_id" do
-    @item_category = ItemCategory.create(:item_id => "")
-    assert @item_category.invalid?
+    item_category = item_categories(:one)
+    item_category.update(:item_id => nil)
+    refute item_category.valid?
   end
 
   test "it_validates_category_id" do
-    @item_category = ItemCategory.create(:item_id => "5", category_id: "")
-    assert @item_category.invalid?
+    item_category = item_categories(:one)
+    item_category.update(:category_id => nil)
+    refute item_category.valid?
   end
 
 end
