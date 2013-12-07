99199a1bd3aa3c5eae5b0d543fb66b6ce2dd63ab
Ben Lewis <bennlewis@gmail.com>
2013-12-07 13:29:45 -0700

    completed updating user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   validates_presence_of     :password, :on => :create
   validates                 :password, length: { minimum: 6 }
-  validates                 :display_name, length: { in: 2..32 }, :allow_blank => true
+  validates                 :display_name, length: { in: 2..32 }
   validates_presence_of     :email
   validates_presence_of     :full_name
+  validates_presence_of     :display_name
   validates_format_of       :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates                 :email, uniqueness: true 
 
--- test/models/users_test.rb
+++ test/models/users_test.rb
@@ -2,33 +2,47 @@ require "test_helper"
 
 class UsersTest < ActiveSupport::TestCase
 
-  test "it_validates_its_attributes" do
-    assert users(:one).invalid?
+  def create_valid_user
+    @user = User.create(email:"teeest@example.com", display_name:"benjammin", full_name:"Benjamin B Lewis, esq.", password:"password", password_confirmation: "password")
+  end
+
+  def setup
+    create_valid_user
+  end
+
+  test "it is valid with valid attributes" do
+    assert @user.valid?
   end
 
   test "it validates email" do
-    user = users(:one)
-    user.update(email: nil)
-    refute user.valid?
+    @user.update(email: nil)
+    refute @user.valid?
   end
 
  
   test "it_validates_full_name" do
-    user = users(:one)
-    user.update(full_name: nil)
-    user.update(:full_name => nil)
-    refute user.valid?
+    @user.update(full_name: nil)
+    refute @user.valid?
   end
 
   test "it_validates_display_name" do 
-    user = users(:one)
-    user.update(display_name: nil)
-    user.update(:display_name => nil)
-    refute user.valid?
+    @user.update(display_name: nil)
+    refute @user.valid?
+  end 
+
+  test "it_is_not_a_super_by_default" do 
+    refute @user.super
+    @user.update(super: true)
+    assert @user.super
   end 
 
   test "it can have one or more orders" do
-    assert users(:one).orders.count == 2
+    order = orders(:one)
+    order.update(user: @user)
+    order2 = orders(:two)
+    order2.update(user: @user)
+
+    assert @user.orders.count == 2
   end
 
 end
