ae39cbdde7c6f389f97d49cf1db181b9fb71e269
Ben Lewis <bennlewis@gmail.com>
2013-12-07 13:10:15 -0700

    updated user fixtures to use password hash, fixed logging in and out test

--- test/features/can_log_in_and_out_test.rb
+++ test/features/can_log_in_and_out_test.rb
@@ -3,35 +3,36 @@ require './test/test_helper'
 class CanLogInAndOutTest < Capybara::Rails::TestCase
 
   test "user can sign up with valid params" do
-    visit '/onoburrito'
-    click_on "Sign up or Log in"
+    visit root_path
 
+    click_on "Sign up or Log in"
     within "#new_user" do
       fill_in "Email", with: 'benji@example.com'
       fill_in "Full name", with: 'Benjamin Franklin'
-      fill_in "Display name", with: "bennybeans"
+      fill_in "Display name", with: 'benitobeans'
       fill_in "Password", with: 'password'
       fill_in "Password confirmation", with: 'password'
       click_button "Create User"
     end
     assert_content page, 'Logged in'
+  end
+
+  test "test can log out" do
+    visit root_path
+
+    click_on "Sign up or Log in"
+    within "#login-form" do
+      fill_in "Email", with: 'benji@example.com'
+      fill_in "Password", with: 'password'
+      click_button "Log In"
     end
 
-    test "test can log out" do
-      user = create_valid_user
-      visit root_path
-      click_on "Sign up or Log in"
-      within "#login-form" do
-        fill_in "Email", with: user.email
-        fill_in "Password", with: "password"
-        click_button "Log In"
-      end
-      click_on "Log out"
-      assert_content page, "Logged out"
-    end
+    click_on "Log out"
+    assert_content page, "Logged out"
+  end
 
-    test "cannot sign up with invalid params" do
-      visit '/onoburrito'
+  test "cannot sign up with invalid params" do
+    visit root_path
 
     click_on "Sign up or Log in"
 
@@ -44,8 +45,3 @@ class CanLogInAndOutTest < Capybara::Rails::TestCase
     assert_content page, "can't be blank"
   end
 end
-
-
-
-
-
