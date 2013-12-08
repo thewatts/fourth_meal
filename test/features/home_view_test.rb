a4bb984e98b5d9131c60716be1c72aac98a4de2b
Ben Lewis <bennlewis@gmail.com>
2013-12-07 22:10:31 -0700

    updated some capybara tests

--- test/features/home_view_test.rb
+++ test/features/home_view_test.rb
@@ -3,13 +3,11 @@ require "test_helper"
 class HomeViewTest < Capybara::Rails::TestCase
 
   test "visit restaurant from home page" do
-    create_valid_restaurant
-
     visit root_path
     assert page.has_content?('All Restaurants')
     click_on "KFC"
 
-    assert_equal restaurant_root_path(@restaurant), current_path
+    assert_equal restaurant_root_path(restaurants(:one)), current_path
     assert page.has_content?("Mashed Potatoes")
   end
 end
--- test/features/menu_test.rb
+++ test/features/menu_test.rb
@@ -4,25 +4,22 @@ class MenuTest < Capybara::Rails::TestCase
 
   def test_guest_can_browse_the_home_page
     visit root_path
-    assert page.has_content?('On the Menu At Ono')
-  end
-
-  def test_guest_can_browse_menu_page
-    visit root_path
-    click_on 'Entrees'
-    within ("#main-body") do
-      assert page.has_css?('#item_1')
-    end
-    assert page.has_css?('.tile-details')
-    assert page.has_link?('Sign up or Log in')
-    assert page.has_css?('.add-to-cart')
+    click_on "KFC"
+    assert page.has_content?('Kentucky Fried Wonder')
   end
 
   def test_guest_can_sort_by_specific_category
-    visit menu_path
-    click_on 'Kids Menu'
-    within('.menu-view') do
-      assert page.has_content?('Kids Taco')
+    visit root_path
+    click_on "KFC"
+    click_on 'Brunch'
+    within ("#main-body") do
+      assert_content page, "#item_#{items(:two).id}"
+      assert_content page, "Mashed Potatoes"
+    end
+    click_on 'Linner'
+    within('#main-body') do
+      assert_content page, 'Chicken Drumsticks'
+      refute_content page, "Mashed Potatoes"
     end
   end
 
