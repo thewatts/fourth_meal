05f98d330d7fc0fe65c10e24795cf460ece2308d
Ben Lewis <bennlewis@gmail.com>
2013-12-07 20:52:09 -0700

    updated logging in and out capybara test

--- test/features/can_make_an_order_test.rb
+++ test/features/can_make_an_order_test.rb
@@ -4,12 +4,10 @@ require './test/test_helper'
 class CanMakeAnOrderTest < Capybara::Rails::TestCase
 
   test "a user can create an order" do
-    create_valid_restaurant
-
     visit root_path
     click_on "KFC"
 
-    within "#item_#{@item.id}" do
+    within "#item_#{items(:two).id}" do
       click_on "Add to Cart"
     end
 
@@ -18,29 +16,15 @@ class CanMakeAnOrderTest < Capybara::Rails::TestCase
     
   end
 
-  
-
   test "can add multiple items to order without logging in" do
-    create_valid_restaurant
-    @item2 = Item.create(:title => "Chicken Drumsticks",
-                            :description => "World",
-                            :price => 4,
-                            :slug => "new_item",
-                            :retired => false,
-                            :photo_file_name => "hello.jpg",
-                            :photo_content_type => "jpeg",
-                            :photo_file_size => 12353,
-                            :photo_updated_at => Time.now.to_s,
-                            :restaurant_id => @restaurant.id)
-
     visit root_path
     click_on "KFC"
 
-    within "#item_#{@item.id}" do
+    within "#item_#{items(:two).id}" do
       click_on "Add to Cart"
     end
 
-    within "#item_#{@item2.id}" do
+    within "#item_#{items(:three).id}" do
       click_on "Add to Cart"
     end
 
@@ -50,15 +34,14 @@ class CanMakeAnOrderTest < Capybara::Rails::TestCase
   end
 
   test "can add multiple instances of same item to order" do
-    create_valid_restaurant
     visit root_path
     click_on "KFC"
 
-    within "#item_#{@item.id}" do
+    within "#item_#{items(:one).id}" do
       click_on "Add to Cart"
     end
 
-    within "#item_#{@item.id}" do
+    within "#item_#{items(:one).id}" do
       click_on "Add to Cart"
     end
 
@@ -67,6 +50,33 @@ class CanMakeAnOrderTest < Capybara::Rails::TestCase
     within("#current_order") do
       assert_content page, "2"
     end
+  end
+
+  test "a user can create an order on multiple stores" do
+    visit root_path
+    click_on "KFC"
+
+    within "#item_#{items(:two).id}" do
+      click_on "Add to Cart"
+    end
+
+    assert_content page, 'Your Current Order'
+    assert_content page, "Mashed Potatoes"
+
+    visit root_path
+    click_on "McDonalds"
+
+    within "#item_#{items(:four).id}" do
+      click_on "Add to Cart"
+    end
+
+    save_and_open_page
+
+    assert_content page, 'Your Current Order'
+    refute_content page, "Mashed Potatoes"
+    assert_content page, "Big Mac"
+
+    
   end
 
 end
