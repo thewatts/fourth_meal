d3c9b36402aa4a0e49064eeb7508d1692cc54a06
Ben Lewis <bennlewis@gmail.com>
2013-12-08 21:55:22 -0700

    removed extra folders from admin views, scoped admin controller to current restaurant

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,16 +3,16 @@ class AdminController < ApplicationController
   layout 'admin'
 
   def index
-    @orders = Order.all 
-    @users = User.all
-    @items = Item.all
-    @order_items = OrderItem.all
+    @orders = current_restaurant.orders.all 
+    @users = current_restaurant.users.all
+    @items = current_restaurant.items.all
+    @order_items = current_restaurant.order_items.all
     @total_sales = total_sales
     render :index
   end
 
   def total_sales
-    completed_orders = Order.where(:status => 'complete')
+    completed_orders = current_restaurant.orders.where(:status => 'complete')
     completed_orders.collect do |order|
       order.total_price
     end.reduce(:+)
