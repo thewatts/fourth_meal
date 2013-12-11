4918823798c597a982c0d8aa8a44ac7ad77766c0
Ben Lewis <bennlewis@gmail.com>
2013-12-10 17:45:02 -0700

    refactored sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,16 +9,9 @@ class SessionsController < ApplicationController
     if @user
       session[:user_id] = @user.id
       flash[:notice] = "Logged in!"
-      if session[:forwarding_path]
-        redirect_to session[:forwarding_path]
-      elsif current_restaurant
-        redirect_to restaurant_root_path(session[:current_restaurant])
-      else
-        redirect_to root_path
-      end
+      find_redirect
     else
-      flash[:notice] = "Invalid email or password"
-      render :new
+      invalid_login_redirect
     end
   end
 
@@ -26,6 +19,23 @@ class SessionsController < ApplicationController
     session[:current_order] = nil
     session[:user_id] = nil
     redirect_to root_url, :notice => "Logged out!"
+  end
+
+  private
+
+  def find_redirect
+    if session[:forwarding_path]
+      redirect_to session[:forwarding_path]
+    elsif current_restaurant
+      redirect_to restaurant_root_path(session[:current_restaurant])
+    else
+      redirect_to root_path
+    end
+  end
+
+  def invalid_login_redirect
+    flash[:notice] = "Invalid email or password"
+    render :new
   end
 
 end
