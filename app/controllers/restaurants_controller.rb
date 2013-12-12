054d002e5444a9478382a818e61954969efa240d
Billy <navyosu@gmail.com>
2013-12-11 22:15:08 -0700

    When a user creates a restaurant, it associates the user with that restaurant and gives them a role of owner

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,4 +8,24 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      @restaurant.restaurant_users.create(:restaurant => @restaurant, 
+                                          :user => current_user, 
+                                          :role => "owner")
+      flash.notice = "Your request has been submitted. 
+                      You will be emailed when your restaurant is approved."
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description)
+  end
+
 end
