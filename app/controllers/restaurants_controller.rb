73d7ee1d10131a7eecbf3f7aa36c57da4133eb37
Billy <navyosu@gmail.com>
2013-12-12 07:37:54 -0700

    Made authorization for creating a new restaurant only allowable for logged in users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,10 +5,12 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    verify_logged_in_user
     @restaurant = Restaurant.new
   end
 
   def create
+    verify_logged_in_user
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
       @restaurant.restaurant_users.create(:restaurant => @restaurant, 
@@ -23,6 +25,13 @@ class RestaurantsController < ApplicationController
   end
 
   private
+
+  def verify_logged_in_user
+    unless current_user
+      redirect_to log_in_path
+      flash.notice = "Please login before attempting to create a new restaurant."
+    end
+  end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :description)
