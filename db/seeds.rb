69f4bf962d2a3f197fa6d4db31c17c0a21dc04f2
Ben Lewis <bennlewis@gmail.com>
2013-12-14 19:27:49 -0700

    put seeds back to small numbers

--- db/seeds.rb
+++ db/seeds.rb
@@ -91,8 +91,8 @@ time = Benchmark.measure do
     end
   end
 
-  # restaurants.each {|r| clone_restaurant(r, cities, 10) }
-  restaurants.each {|r| clone_restaurant(r, cities, 1000) }
+  restaurants.each {|r| clone_restaurant(r, cities, 10) }
+  # restaurants.each {|r| clone_restaurant(r, cities, 1000) }
 
 
 
@@ -154,8 +154,8 @@ time = Benchmark.measure do
     end
   end
 
-  seed_users(100000)
-  # seed_users(100)
+  # seed_users(100000)
+  seed_users(100)
 
 
 
